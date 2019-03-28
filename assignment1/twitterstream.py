0ffbb99bb9ee085c67bd1d1ab200463159a3dd54
Amos Yap <43962539+ahmos01@users.noreply.github.com>
2019-03-28 16:49:58 +0800

    Update twitterstream.py

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -3,10 +3,10 @@ import urllib2 as urllib
 
 # See assignment1.html instructions or README for how to get these credentials
 
-api_key = "<Enter api key>"
-api_secret = "<Enter api secret>"
-access_token_key = "<Enter your access token key here>"
-access_token_secret = "<Enter your access token secret here>"
+api_key = "<5Fw1Dh11OMDoFkyRJDm8JGLKe>"
+api_secret = "<4U9IT7ymjlUECJrvr4O2dUuOctSJYLLtf2I5OW6V7AU4qeXW5f>"
+access_token_key = "<1543504904-oLd4hwSABU7sMKrndVktT0uwX2V8jjayPsEKMuq >"
+access_token_secret = "<9Es5k3a9N5rQY5CiOU1PYaOhBA3rZDElQ03Zgw8NKWWDp>"
 
 _debug = 0
 
